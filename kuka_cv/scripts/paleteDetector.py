df1d7e1ea136812d61aa331e453c918e0359e1d3
Ovcharov Aleksei <senserlex@gmail.com>
2017-11-13 01:20:20 +0300

    Add more transformation to 'camera_link' frame.

--- kuka_cv/scripts/paleteDetector.py
+++ kuka_cv/scripts/paleteDetector.py
@@ -8,6 +8,7 @@ Created on Wed Oct 18 15:46:41 2017
 import roslib
 import rospy
 import tf2_ros
+import tf
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from kuka_cv.msg import Palette
@@ -18,7 +19,7 @@ import cv2
 
 class PaleteFinder:
 
-    def __init__(self, resolution, vectorOfCamera, scaleFactor, paletteThresh, freq):
+    def __init__(self, resolution, cameraPos, cameraRot, scaleFactor, paletteThresh, freq):
         self.bridge = CvBridge();
         self.imageSub = rospy.Subscriber("/Camera", Image, self.callback)
         self.palettePub = rospy.Publisher("/Palette", Palette, queue_size=10)
@@ -29,17 +30,29 @@ class PaleteFinder:
 
         self.width = resolution[0]          # camera resolution [px]
         self.height = resolution[1]         # camera resolution [px]
-        self.dx = vectorOfCamera[0]         # camera in manipulator frame [m]
-        self.dy = vectorOfCamera[1]         # camera in manipulator frame [m]
-        self.dz = vectorOfCamera[2]         # camera in manipulator frame [m]
+        self.dx = cameraPos.x               # camera in manipulator frame [m]
+        self.dy = cameraPos.y               # camera in manipulator frame [m]
+        self.dz = cameraPos.z               # camera in manipulator frame [m]
+        self.rot = cameraRot                # camera Transformation 4x4 matrix
         self.kx = scaleFactor[0]            # scale factor [m/px]
         self.ky = scaleFactor[1]            # scale factor [m/px]
 
     def coordTransform(self, x, y, z):
-        newX = (-y + self.width/2)*self.kx + self.dx
-        newY = (-x + self.height/2)*self.ky + self.dy
-        newZ = z
-        return [round(newX, 4), round(newY, 4), newZ]
+        # First transform
+        # Rotate camera frame to frame of joint_a6 (link_6)
+        x1 = -(y - self.height/2)*self.kx
+        y1 = (x - self.width/2)*self.ky
+        z1 = z
+
+        # print("p: " + str((x1, y1, z1)))
+        # Second transform
+        # Move to vector [dx, dy, dz]
+        # Rotate camera frame to base frame (base_link)
+        newX = self.rot[0, 0]*x1 + self.rot[0, 1]*y1 + self.rot[0, 2]*z1 + self.dx
+        newY = self.rot[1, 0]*x1 + self.rot[1, 1]*y1 + self.rot[1, 2]*z1 + self.dy
+        newZ = self.rot[2, 0]*x1 + self.rot[2, 1]*y1 + self.rot[2, 2]*z1
+
+        return [round(newX, 4), round(newY, 4), round(newZ, 4)]
 
     def callback(self, data):
         try:
@@ -100,11 +113,19 @@ class PaleteFinder:
 def main():
     rospy.init_node("palete_detector")
 
+    # TODO add calibration mode
+    calibration = True
+
     """
     If you don't know vector of camera relatibe manipulator frame set:
         
-        resulotion = [0, 0]
-        vectorOfCamera = [0, 0]
+        cameraPosition.x = 0
+        cameraPosition.y = 0
+        cameraPosition.z = 0
+        cameraOrientation.x = 0
+        cameraOrientation.y = 0
+        cameraOrientation.z = 0
+        cameraOrientation.w = 1
         scaleFactor = [1, 1]
 
     After that by output data compute scale factor and set vector of camera.     
@@ -116,29 +137,33 @@ def main():
     tempRate = rospy.Rate(10)
     while not rospy.is_shutdown():
         try:
-            trans = tfBuffer.lookup_transform("base_link", "link_6", rospy.Time())
-            print(trans.transform.translation)
+            trans = tfBuffer.lookup_transform("base_link", "camera_link", rospy.Time())
+            print(trans)
             break
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             tempRate.sleep()
             continue
 
-    # TODO add camera offsets
     cameraPosition = trans.transform.translation;
-    cameraOffset = [0.1, 0, 0]
-    vectorOfCamera = [cameraPosition.x, cameraPosition.y + cameraOffset[0], cameraPosition.z]
+    cameraRotation = trans.transform.rotation;
+
+    rotMatrix = tf.transformations.quaternion_matrix([cameraRotation.x,
+        cameraRotation.y,
+        cameraRotation.z,
+        cameraRotation.w])
 
     # TODO calculate scale factor, add function to calibration
-    diffPaintDistanceM = [0.05, 0.014]                              # Distance between paints [m]
-    diffPaintDistancePX = [67, 17]                                  # Distance between paints [px]
+    diffPaintDistanceM = [0.05, -0.012]                              # Distance between paints [m]
+    diffPaintDistancePX = [66, -17]                                  # Distance between paints [px]
     scaleFactor = [diffPaintDistanceM[0]/diffPaintDistancePX[0],
                    diffPaintDistanceM[1]/diffPaintDistancePX[1]]    # Scale factor [m/px]
 
+    # TODO add automatic calc of resolution
     resolution = [1280, 720]
     paletteThresh = 230
     freq = 10
 
-    detector = PaleteFinder(resolution, vectorOfCamera, scaleFactor, paletteThresh, freq)
+    detector = PaleteFinder(resolution, cameraPosition, rotMatrix, scaleFactor, paletteThresh, freq)
     try:
         rospy.spin()
     except CvBridgeError as e:
