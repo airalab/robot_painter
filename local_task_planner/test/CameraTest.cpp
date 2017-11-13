171e646f34a1be6d64865b9a020c18e71dfda8c5
Ovcharov Alexei <senserlex@gmail.com>
2017-11-13 19:01:46 +0300

    Add launch file. Add service for set palette detector mode. Find pose for detect colors on palette.

--- local_task_planner/test/CameraTest.cpp
+++ local_task_planner/test/CameraTest.cpp
@@ -3,6 +3,7 @@
 #include <arm_manipulation/Manipulator.h>
 #include <kuka_cv/Palette.h>
 #include <kuka_cv/Colour.h>
+#include <kuka_cv/SetMode.h>
 
 std::vector<kuka_cv::Colour> colours;
 
@@ -25,6 +26,9 @@ int main(int argc, char ** argv)
     // Subscribers
     ros::Subscriber paletteSubscriber = nh.subscribe("Palette", 10, paletteCallback);
 
+    // Service client
+    ros::ServiceClient cameraModeClient = nh.serviceClient<kuka_cv::SetMode>("/SetPaleteDetectionMode");
+
     // Initialize manipulator
     std::string prefix = "joint_a";
     Manipulator manipulator(prefix, nh);
@@ -35,21 +39,38 @@ int main(int argc, char ** argv)
     if (question != "y")
         return 0;
 
+    Pose detectPose;    // Calc by experiment
+    detectPose.position(0) = 0.1;
+    detectPose.position(1) = 0.4;
+    detectPose.position(2) = 0.85;
+    detectPose.orientation(0) = 0.0;
+    detectPose.orientation(1) = 0.0;
+    detectPose.orientation(2) = 1.5708;
+    std::vector<double> config = {0, 1, 1};
+
     JointValues initiatlJV;
     initiatlJV(1) = -M_PI/2;
     initiatlJV(2) = M_PI/2;
+
+    ROS_INFO_STREAM("[LTP] Move to initial pose");
+    manipulator.moveArm(initiatlJV);
+
+    ROS_INFO_STREAM("[LTP] Move to detect pose");
+    manipulator.moveArm(detectPose, config);
+
+    kuka_cv::SetMode cameraSrv;
+    cameraSrv.request.mode = 1;         // Measuring mode of camera
+    ROS_INFO_STREAM("[LTP] Turn on camera and detect palette: ");
+    if (cameraModeClient.call(cameraSrv)) {
+        ROS_INFO_STREAM("Successful");
+    }
 
     ROS_INFO_STREAM("[LTP] Receive palette message.");
     while (colours.size() == 0)
         ros::spinOnce();
 
-    ROS_INFO_STREAM("[LTP] Move to initial pose");
-    manipulator.moveArm(initiatlJV);
-    ros::Duration(5).sleep();
-
     ROS_INFO_STREAM("[LTP] Move to colours on palette");
     Pose p;
-    std::vector<double> config = {0, 1, 1};
     for (size_t i = 0; i < colours.size(); ++i) {
         ROS_INFO_STREAM("[LTP] Number: " << (i + 1) << " --------------");
         ROS_INFO_STREAM("[LTP] Colour: [" << colours[i].bgr[0] 
