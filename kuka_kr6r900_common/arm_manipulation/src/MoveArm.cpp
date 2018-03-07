fe4f1112260379b174588d94e9fd51d06834b5ea
Aleksei Ovcharov <senserlex@gmail.com>
2018-03-07 04:39:58 +0300

    Added move to angle

--- kuka_kr6r900_common/arm_manipulation/src/MoveArm.cpp
+++ kuka_kr6r900_common/arm_manipulation/src/MoveArm.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
     ros::NodeHandle nh;
 
     // Initialize manipulator
+    JointValues jv;
     std::string prefix = "joint_a";
     Manipulator manipulator(prefix, nh);
     manipulator.initArmTopics();
@@ -15,23 +16,27 @@ int main(int argc, char *argv[])
     Pose pose;
     double x = 0.525, y = 0, z = 0.89, alpha = 0, beta = 0, gamma = 0;
     std::vector<double> config = {0, 1, 1};
-    std::string question;
+    std::string answer;
 
-    std::cout << "Do you wont move the arm? (y, n): "; std::cin >> question;
-    while(nh.ok() && (question == "y" || question == "\n")) {
-        std::cout << "Position: "; std::cin >> x >> y >> z;
-        std::cout << "Orientation: "; std::cin >> alpha >> beta >> gamma;
-        std::cout << "Configuration: "; std::cin >> config[0] >> config[1] >> config[2];
+    std::cout << "Do you wont move the arm? (pos, ang, n): "; std::cin >> answer;
+    while(nh.ok() && answer != "n") {
+        if (answer == "pos") {
+            std::cout << "Position: "; std::cin >> x >> y >> z;
+            std::cout << "Orientation: "; std::cin >> alpha >> beta >> gamma;
+            std::cout << "Configuration: "; std::cin >> config[0] >> config[1] >> config[2];
 
-        pose.position(0) = x;
-        pose.position(1) = y;
-        pose.position(2) = z;
-        pose.orientation(0) = alpha;
-        pose.orientation(1) = beta;
-        pose.orientation(2) = gamma;
+            pose.position(0) = x;
+            pose.position(1) = y;
+            pose.position(2) = z;
+            pose.orientation(0) = alpha;
+            pose.orientation(1) = beta;
+            pose.orientation(2) = gamma;
 
-        manipulator.moveArm(pose, config);
-        std::cout << "Do you wont move the arm? (y, n): "; std::cin >> question;
+            manipulator.moveArm(pose, config);
+        } else if (answer == "ang") {
+            std::cout << "ang: "; std::cin >> jv(0) >> jv(1) >> jv(2) >> jv(3) >> jv(4) >> jv(5);
+        }
+        std::cout << "Do you wont move the arm? (y, n): "; std::cin >> answer;
     }
     return 0;
 }
