d401176986010ea048b18a85877c41ee93c55209
Aleksei Ovcharov <senserlex@gmail.com>
2018-07-05 08:43:56 +0300

    Added 'joint_states' topic tracking

--- kuka_kr6r900_common/arm_manipulation/src/Manipulator.cpp
+++ kuka_kr6r900_common/arm_manipulation/src/Manipulator.cpp
@@ -1,5 +1,6 @@
 #include "arm_manipulation/Manipulator.h"
-#define MANIPULATION_DEBUG false 
+#define MANIPULATION_DEBUG false
+#define JOINT_STATE true
 // #define OFFSET_A1 0
 // #define OFFSET_A2 -90
 // #define OFFSET_A3 90
@@ -19,7 +20,8 @@ void Manipulator::initArmTopics()
     ROS_INFO_STREAM("[AM] Load topics ...");
     ROS_INFO_STREAM("[AM] Publisher: /kuka_arm/arm_controller/position_command" << "\n");
     armPublisher = nh.advertise<brics_actuator::JointPositions> ("/kuka_arm/arm_controller/position_command", 1);
-    // jointStateSubscriber = nh.subscribe("/joint_states", 10, &Manipulator::stateCallback, this);
+    if (JOINT_STATE)
+        jointStateSubscriber = nh.subscribe("/joint_states", 10, &Manipulator::stateCallback, this);
     ros::Duration(1).sleep();
 }
 
@@ -38,7 +40,7 @@ bool Manipulator::moveArm(JointValues jointValues)
     // jointValues(4) = jointValues(4) * 180/M_PI - OFFSET_A5;
     // jointValues(5) = jointValues(5) * 180/M_PI - OFFSET_A6;
     brics_actuator::JointPositions jointPositions = createArmPositionMsg(jointPrefix, jointValues);
-    
+
     if (MANIPULATION_DEBUG)
         ROS_INFO_STREAM("[AM] Move to pose.");
 
@@ -50,28 +52,31 @@ bool Manipulator::moveArm(JointValues jointValues)
 
     armPublisher.publish(jointPositions);
 
-    // JointValues diff;
-    // JointValues prevState;
-    // bool check;
-    // do {
-    //     prevState = jointState;
-    //     do {
-    //         ros::spinOnce();
-    //         for (size_t i = 0; i < DOF; ++i) {
-    //             check = check && (jointState(i) == prevState(i));
-    //         }
-    //     } while(check && nh.ok());
-    //     diff = jointState - jointValues;
-    // } while(diff.norm() > 0.01 && nh.ok());
+    if (JOINT_STATE) {
+        // Check position
+        JointValues diff;
+        JointValues prevState;
+        bool check;
+        do {
+            prevState = jointState;
+            do {
+                ros::spinOnce();
+                for (size_t i = 0; i < DOF; ++i) {
+                    check = check && (jointState(i) == prevState(i));
+                }
+            } while(check && nh.ok());
+            diff = jointState - jointValues;
+        } while(diff.norm() > 0.05 && nh.ok());
+    }
 
     return true;
 }
 bool Manipulator::moveArm(const Pose & pose, const std::vector<double> & configuration)
 {
     JointValues jointAngles;
-    
+
     // All configurations for manipulator
-    std::vector<std::vector<double>> config = {{0, 1, 1}, {0, 1, -1}, {0, -1, 1}, {0, -1, -1}, 
+    std::vector<std::vector<double>> config = {{0, 1, 1}, {0, 1, -1}, {0, -1, 1}, {0, -1, -1},
         {M_PI, 1, 1}, {M_PI, 1, -1}, {M_PI, -1, 1}, {M_PI, -1, -1}};
 
     if (solver.solveIK(pose, configuration, jointAngles)) {
@@ -85,9 +90,9 @@ bool Manipulator::moveArm(const Pose & pose, const std::vector<double> & configu
         }
     }
 
-    ROS_ERROR_STREAM("[AM] Solution for pose (" 
+    ROS_ERROR_STREAM("[AM] Solution for pose ("
         << pose.position(0) << ", "
-        << pose.position(1) << ", " 
+        << pose.position(1) << ", "
         << pose.position(2) << ") is NOT FOND");
 }
 
@@ -103,7 +108,7 @@ void Manipulator::stateCallback(const sensor_msgs::JointStatePtr & msg) {
 
 brics_actuator::JointPositions createArmPositionMsg(std::string prefix, JointValues jointAngles)
 {
-    brics_actuator::JointPositions jointPositions; 
+    brics_actuator::JointPositions jointPositions;
     brics_actuator::JointValue jointValue;
 
     for (size_t i = 0; i < DOF; ++i) {
@@ -112,7 +117,7 @@ brics_actuator::JointPositions createArmPositionMsg(std::string prefix, JointVal
         jointName << prefix << (i + 1);
         jointValue.joint_uri = jointName.str();
         jointValue.unit = "rad";
-        jointValue.value = jointAngles(i); 
+        jointValue.value = jointAngles(i);
         jointPositions.positions.push_back(jointValue);
     }
     return jointPositions;
