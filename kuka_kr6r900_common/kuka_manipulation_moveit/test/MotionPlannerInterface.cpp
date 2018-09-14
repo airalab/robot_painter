8fbab52e366ae930c962b879fbfdb9b9465225c5
Aleksei Ovcharov <senserlex@gmail.com>
2018-09-14 12:36:02 +0300

    Hieroglyph v0

---
+++ kuka_kr6r900_common/kuka_manipulation_moveit/test/MotionPlannerInterface.cpp
@@ -0,0 +1,188 @@
+#include <pluginlib/class_loader.h>
+#include <ros/ros.h>
+
+// MoveIt!
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_interface/planning_interface.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/kinematic_constraints/utils.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/PlanningScene.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
+#include <moveit/trajectory_processing/iterative_time_parameterization.h>
+#include <moveit/trajectory_processing/iterative_spline_parameterization.h>
+
+#include <boost/scoped_ptr.hpp>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "motion_planning_tutorial");
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+    ros::NodeHandle node_handle("planning_test");
+
+    const std::string PLANNING_GROUP = "manipulator";
+    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+    robot_model::RobotModelPtr robot_model = robot_model_loader.getModel();
+
+    /* Create a RobotState and JointModelGroup to keep track of the current robot pose and planning group*/
+    robot_state::RobotStatePtr robot_state(new robot_state::RobotState(robot_model));
+    const robot_state::JointModelGroup* joint_model_group = robot_state->getJointModelGroup(PLANNING_GROUP);
+
+    // Using the :moveit_core:`RobotModel`, we can construct a
+    // :planning_scene:`PlanningScene` that maintains the state of
+    // the world (including the robot).
+    planning_scene::PlanningScenePtr planning_scene(new planning_scene::PlanningScene(robot_model));
+
+    // We will now construct a loader to load a planner, by name.
+    // Note that we are using the ROS pluginlib library here.
+    boost::scoped_ptr<pluginlib::ClassLoader<planning_interface::PlannerManager>> planner_plugin_loader;
+    planning_interface::PlannerManagerPtr planner_instance;
+    std::string planner_plugin_name = "ompl_interface/OMPLPlanner";
+
+    try {
+        planner_plugin_loader.reset(new pluginlib::ClassLoader<planning_interface::PlannerManager>(
+            "moveit_core", "planning_interface::PlannerManager"));
+    } catch (pluginlib::PluginlibException& ex) {
+        ROS_FATAL_STREAM("Exception while creating planning plugin loader " << ex.what());
+    }
+
+    try {
+        planner_instance.reset(planner_plugin_loader->createUnmanagedInstance(planner_plugin_name));
+        if (!planner_instance->initialize(robot_model, node_handle.getNamespace()))
+        ROS_FATAL_STREAM("Could not initialize planner instance");
+        ROS_INFO_STREAM("Using planning interface '" << planner_instance->getDescription() << "'");
+    } catch (pluginlib::PluginlibException& ex) {
+
+        const std::vector<std::string>& classes = planner_plugin_loader->getDeclaredClasses();
+        std::stringstream ss;
+        for (std::size_t i = 0; i < classes.size(); ++i)
+            ss << classes[i] << " ";
+
+        ROS_ERROR_STREAM("Exception while loading planner '" << planner_plugin_name << "': " << ex.what() << std::endl
+                        << "Available plugins: " << ss.str());
+    }
+
+    // Visualization
+    // ^^^^^^^^^^^^^
+    // The package MoveItVisualTools provides many capabilties for visualizing objects, robots,
+    // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script
+    namespace rvt = rviz_visual_tools;
+    moveit_visual_tools::MoveItVisualTools visual_tools("base_link");
+    visual_tools.deleteAllMarkers();
+
+    /* Remote control is an introspection tool that allows users to step through a high level script
+    via buttons and keyboard shortcuts in RViz */
+    visual_tools.loadRemoteControl();
+
+    /* RViz provides many types of markers, in this demo we will use text, cylinders, and spheres*/
+    Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
+    text_pose.translation().z() = 1.75;
+    visual_tools.publishText(text_pose, "Motion Planning API Demo", rvt::WHITE, rvt::XLARGE);
+
+    /* Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations */
+    visual_tools.trigger();
+
+    /* We can also use visual_tools to wait for user input */
+    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
+
+    // Pose Goal
+    // ^^^^^^^^^
+    // We will now create a motion plan request for the arm of the Panda
+    // specifying the desired pose of the end-effector as input.
+    planning_interface::MotionPlanRequest req;
+    planning_interface::MotionPlanResponse res;
+    geometry_msgs::PoseStamped pose;
+    pose.header.frame_id = "base_link";
+    pose.pose.position.x = 0.5;
+    pose.pose.position.y = 0.0;
+    pose.pose.position.z = 0.75;
+    pose.pose.orientation.w = 1.0;
+
+    // A tolerance of 0.01 m is specified in position
+    // and 0.01 radians in orientation
+    std::vector<double> tolerance_pose(3, 0.01);
+    std::vector<double> tolerance_angle(3, 0.01);
+
+    // We will create the request as a constraint using a helper function available
+    // from the
+    // `kinematic_constraints`_
+    // package.
+    //
+    // .. _kinematic_constraints:
+    //     http://docs.ros.org/indigo/api/moveit_core/html/namespacekinematic__constraints.html#a88becba14be9ced36fefc7980271e132
+    req.group_name = "manipulator";
+    moveit_msgs::Constraints pose_goal =
+    kinematic_constraints::constructGoalConstraints("tool0", pose, tolerance_pose, tolerance_angle);
+    req.goal_constraints.push_back(pose_goal);
+
+    // We now construct a planning context that encapsulate the scene,
+    // the request and the response. We call the planner using this
+    // planning context
+    planning_interface::PlanningContextPtr context =
+    planner_instance->getPlanningContext(planning_scene, req, res.error_code_);
+    context->solve(res);
+    if (res.error_code_.val != res.error_code_.SUCCESS)
+    {
+        ROS_ERROR("Could not compute plan successfully");
+        return 0;
+    }
+
+    // Visualize the result
+    // ^^^^^^^^^^^^^^^^^^^^
+    ros::Publisher display_publisher =
+    node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+    moveit_msgs::DisplayTrajectory display_trajectory;
+
+    /* Visualize the trajectory */
+    ROS_INFO("Visualizing the trajectory");
+    moveit_msgs::MotionPlanResponse response;
+    res.getMessage(response);
+
+    display_trajectory.trajectory_start = response.trajectory_start;
+    display_trajectory.trajectory.push_back(response.trajectory);
+    display_publisher.publish(display_trajectory);
+    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
+
+    // Trajectory parametization
+    robot_trajectory::RobotTrajectory robotTraj = robot_trajectory::RobotTrajectory(*res.trajectory_);
+    moveit_msgs::RobotTrajectory robotTrajMsg;
+
+    double accelScalingFactor = 0.7;
+    double velScalingFactor = 1.0;
+
+    // Parabolic parametrization
+    // trajectory_processing::IterativeSplineParameterization isp;
+    // if (!isp.computeTimeStamps(robotTraj, velScalingFactor, accelScalingFactor))
+    //     return 1;
+
+    // Parabolic parametrization
+    trajectory_processing::IterativeParabolicTimeParameterization iptp;
+    if (!iptp.computeTimeStamps(robotTraj))
+        return 1;
+    robotTraj.getRobotTrajectoryMsg(robotTrajMsg);
+
+    /* Write trajectory to file */
+    trajectory_msgs::JointTrajectory printedTrajectory = robotTrajMsg.joint_trajectory;
+
+    std::string dataProcessingPackagePath = ros::package::getPath("rsi_tests");
+    std::stringstream csvfilePath;
+    csvfilePath << dataProcessingPackagePath << "/data/data1.csv";
+    ROS_INFO("File path: %s", csvfilePath.str().c_str());
+    boost::filesystem::ofstream ofs{csvfilePath.str()};
+    csvfilePath.str("");
+
+    for (size_t i = 0; i < printedTrajectory.points.size(); ++i) {
+
+        // Write to file
+        ofs << printedTrajectory.points[i].time_from_start;
+        for (size_t joint = 0; joint < 6; ++joint)
+            ofs << ", " << printedTrajectory.points[i].positions[joint];
+        ofs << "\n";
+
+    }
+    ofs.close();
+
+    return 0;
+
+}
